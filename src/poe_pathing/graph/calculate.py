666e2b1c554d8898633d15803c7e65fd2bff6112
Brendan He <brendan.he@dash.com.au>
2026-04-01 16:49:11 +1100

    added bfs pathfinding for shortest paths

--- src/poe_pathing/graph/calculate.py
+++ src/poe_pathing/graph/calculate.py
@@ -3,8 +3,27 @@ from build import load_adj
 
 adj = load_adj("../data/skilltree-export_3.28.0.json")
 
+# bfs, returns the path from src to dst
 def shortest_path(src, dst):
-    pass
+    visited = set()
+    queue = [(src, [src])]
+    
+    while queue:
+        current, path = queue.pop(0)
+        if current == dst:
+            return path
+        
+        if current in visited:
+            continue
+        visited.add(current)
+        
+        for neighbor in adj.get(current, []):
+            if neighbor not in visited:
+                queue.append((neighbor, path + [neighbor]))
+    
+    return None
 
+# bfs to find distance of shortest path
 def distance(src, dst):
-    pass
+    path = shortest_path(src, dst)
+    return len(path) - 1 if path else None
