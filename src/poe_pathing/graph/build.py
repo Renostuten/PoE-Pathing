c2979055b85dc528f73597129087048396f19861
Brendan <brendanhe666@gmail.com>
2026-04-27 16:19:56 +1000

    added mastery checks to remove mastery nodes

--- src/poe_pathing/graph/build.py
+++ src/poe_pathing/graph/build.py
@@ -7,12 +7,13 @@ with open("../data/skilltree-export_3.27.0.json", "r", encoding="utf-8") as f:
 # Build adjacency list of the passive tree graph
 adj = defaultdict(list)
 
-# v1: ignores nodes with no connections
+# v1: ignores nodes with no connections (mastery nodes)
 for node_id, node in data["nodes"].items():
     in_nodes = node.get("in", [])
     out_nodes = node.get("out", [])
+    is_mastery = node.get("isMastery", False)
 
-    if not in_nodes and not out_nodes:
+    if (not in_nodes and not out_nodes) or is_mastery:
         continue
 
     # Add edges in both directions (undirected graph)
