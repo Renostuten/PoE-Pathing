6e8e9731ff4de74d7abdbb7335f2f290fe41bf19
Brendan He <brendan.he@dash.com.au>
2026-04-29 13:16:37 +1000

    updated pathfinder class to contain logic on class root node handling

--- src/poe_pathing/graph/pathfinder.py
+++ src/poe_pathing/graph/pathfinder.py
@@ -1,8 +1,10 @@
 from collections import deque
 
 class PathFinder:
-    def __init__(self, adj):
+    def __init__(self, adj, node_lookup, start_root_id):
         self.adj = adj
+        self.node_lookup = node_lookup
+        self.start_root_id = start_root_id
 
     def shortest_path(self, src, dst):
         visited = set()
@@ -20,6 +22,9 @@ class PathFinder:
             visited.add(current)
 
             for neighbor in self.adj.get(current, []):
+                if not self._is_traversable(neighbor):
+                    continue
+
                 if neighbor not in visited:
                     queue.append((neighbor, path + [neighbor]))
 
@@ -28,3 +33,27 @@ class PathFinder:
     def distance(self, src, dst):
         path = self.shortest_path(src, dst)
         return len(path) - 1 if path else None
+    
+    def _is_class_start(self, node: dict) -> bool:
+        return node["classStartIndex"] is not None
+
+
+    def _is_traversable(self, node_id: str) -> bool:
+        node = self.node_lookup.get(node_id)
+
+        if node is None:
+            return False
+
+        if self._is_class_start(node):
+            return node_id == self.start_root_id
+
+        return True
+
+
+    def _is_allocatable(self, node_id: str) -> bool:
+        node = self.node_lookup.get(node_id)
+
+        if node is None:
+            return False
+
+        return not node.get("isClassStart", False)
